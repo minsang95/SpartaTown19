cb346b56cf963b8b258ced9cfd5af2d8c9fdbea6
minsang95 <154496356+minsang95@users.noreply.github.com>
2024-01-23 21:25:06 +0900

    fix/참석인원UI
    플레이어 네임 첫 입력이 목록에 설정 안되던 문제 수정

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -40,6 +40,8 @@ public class GameManager : MonoBehaviour
 
         PlayerAnimator.runtimeAnimatorController = character.AnimatorController;
         PlayerName.text = name;
+        BefName = name + "\n";
+        Persons.Append(BefName);
     }
 
 }
